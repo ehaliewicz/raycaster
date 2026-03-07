c6dac5e43d5e1743f17b194d1b0a96a7af618a25
Erik Haliewicz <ehal256@gmail.com>
2026-03-07 14:58:04 -0800

    use threadpool wait instead of hand-coded spinlock

--- src/platform_win.c
+++ src/platform_win.c
@@ -369,93 +369,87 @@ static thread_pool* thread_pool_create_inner(int cpu_threads)
     assert(cpu_threads > 0);
     thread_pool* tp = (thread_pool*)calloc(1, sizeof(thread_pool));
 
-    if (tp) {
+    InitializeThreadpoolEnvironment(&tp->callback_environ);
 
-        InitializeThreadpoolEnvironment(&tp->callback_environ);
+    tp->pool = CreateThreadpool(NULL);
 
-        tp->pool = CreateThreadpool(NULL);
+    SetThreadpoolThreadMinimum(tp->pool, cpu_threads);
+    SetThreadpoolThreadMaximum(tp->pool, cpu_threads);
 
-        SetThreadpoolThreadMinimum(tp->pool, cpu_threads);
-        SetThreadpoolThreadMaximum(tp->pool, cpu_threads);
+    tp->cleanup_group = CreateThreadpoolCleanupGroup();
 
-        tp->cleanup_group = CreateThreadpoolCleanupGroup();
-
-        SetThreadpoolCallbackPool(&tp->callback_environ, tp->pool);
-        SetThreadpoolCallbackCleanupGroup(&tp->callback_environ, tp->cleanup_group, NULL);
-    }
+    SetThreadpoolCallbackPool(&tp->callback_environ, tp->pool);
+    SetThreadpoolCallbackCleanupGroup(&tp->callback_environ, tp->cleanup_group, NULL);
 
     return tp;
 }
 
-static void thread_pool_add_work_inner(thread_pool* tp, PTP_WORK_CALLBACK function, void* arg_var)
+static PTP_WORK thread_pool_add_work_inner(thread_pool* tp, PTP_WORK_CALLBACK function, void* arg_var)
 {
-    if (tp)
-    {
-        PTP_WORK work = CreateThreadpoolWork(function, arg_var, &tp->callback_environ);
-        SubmitThreadpoolWork(work);
-    }
+    PTP_WORK work = CreateThreadpoolWork(function, arg_var, &tp->callback_environ);
+    SubmitThreadpoolWork(work);
+    return work;
 }
 
 static void thread_pool_destroy_inner(thread_pool* tp)
 {
-    if (tp)
-    {
-        CloseThreadpoolCleanupGroupMembers(tp->cleanup_group, FALSE, NULL);
-        CloseThreadpoolCleanupGroup(tp->cleanup_group);
+    CloseThreadpoolCleanupGroupMembers(tp->cleanup_group, FALSE, NULL);
+    CloseThreadpoolCleanupGroup(tp->cleanup_group);
 
-        DestroyThreadpoolEnvironment(&tp->callback_environ);
+    DestroyThreadpoolEnvironment(&tp->callback_environ);
 
-        CloseThreadpool(tp->pool);
+    CloseThreadpool(tp->pool);
 
-        free(tp);
-    }
+    free(tp);
 }
 
 
 typedef struct {
     void (*fp)(void*);
     void* args;
-    volatile s64 finished;
 } thread_func_and_args;
 
 void CALLBACK thread_caller(PTP_CALLBACK_INSTANCE instance, PVOID arg_var_name, PTP_WORK work) {
     thread_func_and_args* fargs = (thread_func_and_args*)arg_var_name;
     fargs->fp(fargs->args);
-    InterlockedIncrement64(&fargs->finished);
 }
 
 static thread_pool* tp;
 #define MAX_JOB_SLOTS 16
 thread_func_and_args job_slots[MAX_JOB_SLOTS];
+typedef struct {
+    int in_use;
+    PTP_WORK work;
+} work_handle;
+work_handle work_handles[MAX_JOB_SLOTS];
 
 void platform_init_threadpool(int num_threads) {
     for(int i = 0; i < MAX_JOB_SLOTS; i++) {
-        job_slots[i].finished = 1;
+        work_handles[i].in_use = 0;
     }
     tp = thread_pool_create_inner(num_threads);
 }
 
 void platform_add_task(void (*fp)(void* arg), void* arg_ptr) {
     for(int i = 0; i < MAX_JOB_SLOTS; i++) {
-        if(job_slots[i].finished) {
+        if(work_handles[i].in_use == 0) {
             job_slots[i].args = arg_ptr;
             job_slots[i].fp = fp;
-            job_slots[i].finished = 0;
-            thread_pool_add_work_inner(tp, thread_caller, &job_slots[i]);
+            PTP_WORK work_obj = thread_pool_add_work_inner(tp, thread_caller, &job_slots[i]);
+            work_handles[i].in_use = 1;
+            work_handles[i].work = work_obj;
             return;
         }
     }
 }
 
 void platform_join_threadpool() {
-    while(1) {
-        retry:
-
-        for(int i = 0; i < MAX_JOB_SLOTS; i++) {
-            if(job_slots[i].finished == 0) {
-                goto retry;
-            }
+    // wait on all in_use work slots
+    for(int i = 0; i < MAX_JOB_SLOTS; i++) {
+        if(work_handles[i].in_use) {
+            WaitForThreadpoolWorkCallbacks(work_handles[i].work, FALSE);
+            CloseThreadpoolWork(work_handles[i].work);
+            work_handles[i].in_use = 0;   
         }
-        break;
     }
 }
